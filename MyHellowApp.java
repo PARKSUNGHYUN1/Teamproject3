8b3b5ce3f5031d000e42c87ce69c02b56e068f42
PARKSUNGHYUN1 <smecpat@nate.com>
2019-11-11 13:21:55 +0900

    MyHelloFrame, MyHelloApp

--- MyHellowApp.java
+++ MyHellowApp.java
@@ -1,4 +1,5 @@
 
+import javax.swing.*;
 /**
  * 여기에 MyHellowApp 클래스 설명을 작성하십시오.
  * 
@@ -7,27 +8,8 @@
  */
 public class MyHellowApp
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * MyHellowApp 클래스의 객체 생성자
-     */
-    public MyHellowApp()
+    public static void main(String[] args)
     {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+        JFrame mf = new MyHellowFrame();
     }
 }
--- MyHellowFrame.java
+++ MyHellowFrame.java
@@ -1,33 +1,19 @@
 
+import javax.swing.*;
+import java.awt.*;
 /**
  * 여기에 MyHellowFrame 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyHellowFrame
+public class MyHellowFrame extends JFrame
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * MyHellowFrame 클래스의 객체 생성자
-     */
     public MyHellowFrame()
     {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+        this.setSize(300, 300);
+        this.setTitle("실습_3(2019.11.11)");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 }
